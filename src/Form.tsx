6cfb28793785eee416758a69d2ca2f5db286eb00
AustrianMethod <ivanov.nv.spb@gmail.com>
2024-11-12 14:47:27 +0300

    done

--- src/Form.tsx
+++ src/Form.tsx
@@ -10,9 +10,14 @@ export function Form() {
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        console.log(e.currentTarget)
         const formData = new FormData(e.currentTarget);
         const date = formData.get("date") as string;
         const distance = parseFloat(formData.get("distance") as string);
+        if (isNaN(distance)) {
+          console.error("Invalid distance value");
+          return; 
+        }
 
         const newEvent: Form = { date, distance };
         setList((prevList) => ([
@@ -23,7 +28,7 @@ export function Form() {
     }
 
     return (
-        <>
+        <div className="wrapper">
           <form className="form" autoComplete="off" onSubmit={handleSubmit}>
             <div>
               <label htmlFor="date">Дата (ДД.ММ.ГГ)</label>
@@ -31,20 +36,26 @@ export function Form() {
             </div>
             <div>
               <label htmlFor="distance">Пройдено км</label>
-              <input id="distance" name="date" type="text" />
+              <input id="distance" name="distance" type="text" />
             </div>
             <button type="submit">Ок</button>
           </form>
+          <div className="table-row">
+            <span>Дата (ДД.ММ.ГГ)</span>
+            <span>Пройдено км</span>
+            <span>Действия</span>
+          </div>
           <div className="table">
             {list.map(
                 (item, index) => (
                     <div key={index} className="table-row">
                         <span>{item.date}</span>
                         <span>{item.distance}</span>
+                        <span>&times;</span>
                     </div>
                 )
             )}
           </div>
-        </> 
+        </div> 
     )
 }
